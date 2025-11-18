6fa701e1dbff64bdb87dcf164ad26fecee222af0
Ramesh Ragav <a2zdirectorys2k@gmail.com>
2025-11-18 21:30:59 +0530

    Initial project upload

---
+++ src/app/components/Header.js
@@ -0,0 +1,69 @@
+<header className="header">
+
+{/* COLUMN 1 – BLF LOGO + TITLE + BUTTERFLY */}
+<div className="blf-col">
+ <div className="relative inline-flex items-center blf-wrap w-fit mx-auto">
+
+    {/* Logo */}
+  <img
+  src="/assets/img/logos/header1/butterfly (2).gif"
+  className="
+  absolute
+  right-[20px]
+  top-[5px]
+
+  max-md:right-[45px]
+  max-md:top-[-10px]
+
+  animate-fly
+"
+
+/>
+
+  </div>
+</div>
+
+
+</div>
+A
+
+  </div>
+</div>
+
+
+
+
+
+
+
+  </div>
+</div>
+
+  {/* COLUMN 2 — FOF */}
+  <div className="fof-col">
+    <img src="/assets/img/logos/header1/fof.jpg" className="fof-logo" />
+    <div className="fof-title">Friends of Friends (FOF)</div>
+  </div>
+
+  {/* COLUMN 3 — A2Z */}
+  <div className="a2z-col">
+    <img src="/assets/img/logos/header1/a2zgroups.jpg" className="a2z-logo" />
+    <div className="a2z-title">A2Z Group Companies</div>
+  </div>
+
+  {/* COLUMN 4 — REGISTER / LOGIN */}
+  <div className="auth-col">
+    <button className="reg-btn">Register</button>
+    <button className="login-btn">Login</button>
+    <div className="forgot">Forgot Password</div>
+  </div>
+
+  {/* COLUMN 5 — LANGUAGE */}
+  <div className="lang-col">
+    <select className="lang-select">
+      <option>🌐 English ▼</option>
+    </select>
+    <div className="lang-title">Choose Your Language</div>
+  </div>
+
+</header>
